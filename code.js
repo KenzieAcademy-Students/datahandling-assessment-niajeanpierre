312308de5d895f58f5a7a63e7e3e73b5edbcc709
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-04-30 16:28:34 +0000

    deploy: 7450de16029a24420b8bf219e5d7fb3a15ba20a3

--- code.js
+++ code.js
@@ -1,4 +1,25 @@
 // Your Code Here!
+let combinedMovieDataSet = movieDetails
+  .map((detail) => {
+    //Find the same movie in the other array
+    let thisMovie = movies.find(
+      (movie) =>
+        detail.title === movie.title &&
+        movie.year === parseInt(detail.release_date)
+    );
+    if (!thisMovie) {
+      return undefined;
+    }
+    return { ...thisMovie, ...detail };
+  })
+  .filter((movie) => movie);
+console.log("Combined Movies:", combinedMovieDataSet);
 
 console.log("Movies:", movies);
+
 console.log("MovieDetails:", movieDetails);
+
+function MovieCard(movie) {
+  // creates the HTML and returns it
+}
+
